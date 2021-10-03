4e195daa058abdc5689a831ca2f44df6765d5cc8
elpinguinofrio <el.pinguino.frio@gmail.com>
2021-10-03 10:27:03 +0300

    moved in ogNFT contract, removed hardset URI

---
+++ contracts/NFT/StableUnitDAOogNFT.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+/*
+      /$$$$$$            /$$$$$$$   /$$$$$$   /$$$$$$
+     /$$__  $$          | $$__  $$ /$$__  $$ /$$__  $$
+    | $$  \__/ /$$   /$$| $$  \ $$| $$  \ $$| $$  \ $$
+    |  $$$$$$ | $$  | $$| $$  | $$| $$$$$$$$| $$  | $$
+     \____  $$| $$  | $$| $$  | $$| $$__  $$| $$  | $$
+     /$$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$  | $$
+    |  $$$$$$/|  $$$$$$/| $$$$$$$/| $$  | $$|  $$$$$$/
+     \______/  \______/ |_______/ |__/  |__/ \______/
+
+*/
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol";
+
+contract StableUnitDAOogNFT is ERC721, ERC721Enumerable, Pausable, AccessControl {
+    using Counters for Counters.Counter;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+    mapping(address => bool) public pausedImmune;
+
+    string baseURI;
+
+    constructor() ERC721("StableUnitDAO ogNFT", "SUDAO") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        // make it possible to mint and burn tokens even when the contract is paused
+        pausedImmune[address(0)] = true;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string calldata uri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = uri;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function setPausedImmune(address _address, bool isImmune) external onlyRole(PAUSER_ROLE) {
+        pausedImmune[_address] = isImmune;
+    }
+
+    function adminMint(address to) public onlyRole(MINTER_ROLE) {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
+
+    // mint batch of NFTs
+    function adminMintBatch(address[] calldata tos) public onlyRole(MINTER_ROLE) {
+        require(tos.length > 0, "You need to provide at least one receiver");
+
+        for (uint i = 0; i < tos.length; i++) {
+            _safeMint(tos[i], _tokenIdCounter.current());
+            _tokenIdCounter.increment();
+        }
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    override(ERC721, ERC721Enumerable)
+    {
+        require(!paused() || pausedImmune[from] || pausedImmune[to], "Contract is paused");
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable, AccessControl)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
