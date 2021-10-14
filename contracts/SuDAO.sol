707cb02e3c3815697017478061f3951a58b35fe4
elpinguinofrio <el.pinguino.frio@gmail.com>
2021-10-14 12:48:52 +0300

    simplified SuDAO token logic with oz libs and dirty fixed tests

--- contracts/SuDAO.sol
+++ contracts/SuDAO.sol
@@ -14,57 +14,72 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./utils/SuAccessControl.sol";
+
 
 /**
  * @title Governance token for StableUnit Decentralized Autonomous Organisation
  */
-contract SuDAO is ERC20, Ownable, ERC20Burnable {
+contract SuDAO is ERC20, ERC20Permit, ERC20Votes, ERC20Burnable, SuAccessControl {
     using SafeERC20 for IERC20;
-    uint256 constant MAX_SUPPLY = 21_000_000 * 1e18;
 
-    constructor(uint256 _initMint) ERC20("StableUnit DAO", "SuDAO") {
+    uint256 public constant MAX_SUPPLY = 21_000_000 * 1e18;
+
+    constructor(uint256 _initMint) ERC20("StableUnit DAO", "SuDAO") ERC20Permit("StableUnit DAO") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
         _mint(msg.sender, _initMint);
     }
 
-    /// @dev Gas cheaper way to implement subset of oz' AccessControl
-    mapping(address => bool) public isMinter;
-
-    modifier onlyMinter() {
-        require(isMinter[msg.sender] || (msg.sender == owner()), "caller is not a minter");
-        _;
+    function mint(address account, uint256 amount) external onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "max supply is exceeded");
+        _mint(account, amount);
     }
 
-    function setMinter(address _minterAddress, bool _isMinter) public onlyOwner {
-        isMinter[_minterAddress] = _isMinter;
+    function setMinter(address _minterAddress, bool _isMinter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (_isMinter) {
+            grantRole(MINTER_ROLE, _minterAddress);
+        } else {
+            revokeRole(MINTER_ROLE, _minterAddress);
+        }
     }
-
-    /// @notice Creates `_amount` token to `_to`.
-    function mint(address _to, uint256 _amount) public onlyMinter {
-        _mint(_to, _amount);
-        require(totalSupply() <= MAX_SUPPLY, "max supply is exceeded");
-    }
-
     /**
      * @notice The owner of the contact can take away tokens accidentally sent to the contract.
      */
-    function rescueTokens(IERC20 _token, address payable _to, uint256 _balance) external onlyOwner {
-        require(_to != address(0), "suDAO: can not send to zero address");
-
-        if (_token == IERC20(address(0))) {
-            // for Ether
-            uint256 totalBalance = address(this).balance;
-            uint256 balance = _balance == 0 ? totalBalance : _balance <= totalBalance ? _balance : totalBalance;
-            _to.transfer(balance);
+    function rescueTokens(IERC20 token) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        // allow to rescue ether
+        if (address(token) == address(0)) {
+            payable(msg.sender).transfer(address(this).balance);
         } else {
-            // any other erc20
-            uint256 totalBalance = _token.balanceOf(address(this));
-            uint256 balance = _balance == 0 ? totalBalance : _balance <= totalBalance ? _balance : totalBalance;
-            require(balance > 0, "suDAO: trying to send 0 balance");
-            _token.safeTransfer(_to, balance);
+            token.safeTransfer(address(msg.sender), token.balanceOf(address(this)));
         }
     }
 
     receive() external payable {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
 }
---
+++ contracts/utils/SuAccessControl.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+/*
+      /$$$$$$            /$$$$$$$   /$$$$$$   /$$$$$$
+     /$$__  $$          | $$__  $$ /$$__  $$ /$$__  $$
+    | $$  \__/ /$$   /$$| $$  \ $$| $$  \ $$| $$  \ $$
+    |  $$$$$$ | $$  | $$| $$  | $$| $$$$$$$$| $$  | $$
+     \____  $$| $$  | $$| $$  | $$| $$__  $$| $$  | $$
+     /$$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$  | $$
+    |  $$$$$$/|  $$$$$$/| $$$$$$$/| $$  | $$|  $$$$$$/
+     \______/  \______/ |_______/ |__/  |__/ \______/
+
+*/
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SuAccessControl is AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+
+        grantRole(DEFAULT_ADMIN_ROLE, newOwner);
+        grantRole(MINTER_ROLE, newOwner);
+
+        revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        revokeRole(MINTER_ROLE, msg.sender);
+    }
+}
