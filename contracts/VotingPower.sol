c43d1aec4d47b5617b31fa3364d47a6fbe5c3bdd
TheNobleDev <piyushg.dev@gmail.com>
2022-01-08 19:16:16 +0530

    Add VotingPower contract

--- contracts/VotingPower.sol
+++ contracts/VotingPower.sol
@@ -16,24 +16,38 @@ import "./dependencies/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "./dependencies/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "./utils/SuAccessControl.sol";
 
-
 /*
  * @title Abstract token that aggregates voting power of all tokens in SuDAO on particular chain
 */
 
 interface IBalanceable {
+
     /**
      * @dev Returns the amount of tokens owned by `account`.
      */
     function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Returns the current amount of votes that `account` has.
+     */
+    function getVotes(address account) external view returns (uint256);
+
+    /**
+     * @dev Returns the delegate that `account` has chosen.
+     */
+    function delegates(address account) external view returns (address);
+
 }
 
 contract VotingPower is IERC20, IERC20Metadata, SuAccessControl {
+
     uint256 MAX_LEN = 50;
+    uint256 MAX_WEIGHT = 10000;
+
     IBalanceable[] public tokens;
-    // TODO: set weight
-    // TODO: set weights limits
     uint256[] public multipliers;
+
+    mapping(address => bool) private _tokens;
 
     string private _name;
     string private _symbol;
@@ -47,42 +61,46 @@ contract VotingPower is IERC20, IERC20Metadata, SuAccessControl {
     }
 
     function addToken(IBalanceable token, uint256 multiplier) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(tokens.length < MAX_LEN, "too many tokens");
+        require(!_tokens[address(token)], "already added");
+
+        _tokens[address(token)] = true;
         tokens.push(token);
         multipliers.push(multiplier);
-        require(tokens.length <= MAX_LEN, "too many tokens");
     }
 
     function deleteToken(uint256 id, IBalanceable token) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 lastId = tokens.length - 1;
         require(tokens[id] == token, "wrong token id");
-        uint256 len = tokens.length;
-        // if id is not the last token
-        if (2 <= len && id < len-1) {
-            // copy the last on of the place of id
-            tokens[id] = tokens[len-1];
-            multipliers[id] = multipliers[len-1];
-        }
+
+        tokens[id] = tokens[lastId];
+        multipliers[id] = multipliers[lastId];
+
+        _tokens[address(token)] = false;
         tokens.pop();
         multipliers.pop();
     }
 
-    // TODO: rename to setWeight
-    function editToken(uint256 id, IBalanceable token, uint256 newMultiplier) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setWeight(uint256 id, IBalanceable token, uint256 newMultiplier) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(tokens[id] == token, "wrong token id");
+        require(newMultiplier <= MAX_WEIGHT, "weight too high");
         multipliers[id] = newMultiplier;
     }
 
     /**
-    * @dev See {IERC20-balanceOf}.
-    */
-    // TODO: gas estimate
-    // TODO: think to rename it to keep compatible with ERC20Votes (or compound) interface of voting power
-    function balanceOf(address account) public view virtual override returns (uint256) {
-        uint256 balance = 0;
+     * @dev Returns the current amount of votes that `account` has.
+     * If account has not delegated their votes, it uses the token balance of the account.
+     * Otherwise, it uses the voting power of the account.
+     */
+    function getVotes(address account) public view returns (uint256 votes) {
         for (uint256 i = 0; i < tokens.length; i++) {
-            // balance = balance + (tokens[i].balanceOf(account) + tokens[i].votingPower()) * multipliers[i];
-            balance = balance + tokens[i].balanceOf(account) * multipliers[i];
+            if(tokens[i].delegates(account) == address(0)) {
+                votes += tokens[i].balanceOf(account) * multipliers[i];
+            }
+            else {
+                votes += tokens[i].getVotes(account) * multipliers[i];
+            }
         }
-        return balance;
     }
 
     // ==================== standard erc20 interface ==============================
@@ -119,12 +137,18 @@ contract VotingPower is IERC20, IERC20Metadata, SuAccessControl {
     }
 
     /**
+    * @dev See {IERC20-balanceOf}.
+    */
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return getVotes(account);
+    }
+
+    /**
     * @dev See {IERC20-totalSupply}.
     */
     function totalSupply() public view virtual override returns (uint256) {
         revert("not implemented");
     }
-
 
     /**
     * @dev Mock {IERC20-transfer}
@@ -144,14 +168,13 @@ contract VotingPower is IERC20, IERC20Metadata, SuAccessControl {
     * @dev Mock {IERC20-approve}.
     */
     function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        revert("not possible to transfer voting token");
+        revert("not possible to transfer this token");
     }
 
     /**
     * @dev Mock {IERC20-transferFrom}.
     */
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        revert("not possible to transfer vested token");
+        revert("not possible to transfer this token");
     }
-
 }
