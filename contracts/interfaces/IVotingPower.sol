3979d0c931e856d194454a8891a859f0a45c18ed
Дмитрий <mitya-kudr@mail.ru>
2023-05-20 14:01:43 +0300

    feat: update VotingPower

---
+++ contracts/interfaces/IVotingPower.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+
+interface IVotingPower is IERC20, IERC20Metadata, IVotesUpgradeable {
+    error UnavailableFunctionalityError();
+    error WrongArgumentsError();
+    error BaseAssumptionError();
+    error BadTokenInstance();
+
+    function setTokenWeight(address _token, uint256 _weight) external;
+}
